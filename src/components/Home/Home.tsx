78b4db1dbb2a8bac9593eded28f5be0938744208
Jozys <slaar@teckdigital.de>
2021-07-09 19:25:34 +0200

    More Home Layout + Startet Project Site

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { makeStyles, Theme, Typography } from "@material-ui/core";
 import {
   Timeline,
   TimelineContent,
@@ -9,15 +9,16 @@ import {
 import React from "react";
 import Typing from "react-typing-animation";
 import Image from "../../assets/image.png";
+import Image2 from "../../assets/image2.jpg";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   text: {
     fontSize: "8vh",
     fontWeight: "bold",
     alignSelf: "flex-start",
   },
   info_text: {
-    fontSize: "2vh",
+    fontSize: "2.5vh",
     color: "#fff",
     textAlign: "center",
     width: window.innerWidth * 0.3,
@@ -28,7 +29,21 @@ const useStyles = makeStyles(() => ({
     alignSelf: "center",
   },
   image: {
-    transform: "scale(0.95) rotate(-0.025turn) translate(-50%, -20%)",
+    transform: "scale(0.95) rotate(-0.025turn) translate(-10%, -10%)",
+    borderRadius: 10,
+  },
+  image_left: {
+    transform: "scale(0.95) rotate(-0.025turn)",
+    borderRadius: 10,
+  },
+  image_right: {
+    transform: "rotate(0.025turn) translate(-10%, -10%)",
+    borderRadius: 10,
+  },
+  projectLink: {
+    color: "#fb6340",
+    fontWeight: "bold",
+    textDecorationLine: "none",
   },
 }));
 
@@ -49,17 +64,16 @@ export default function Home(props: HomeProps): JSX.Element {
             <h3 className={styles.title}> Who am I?</h3>
             <Typography className={styles.info_text}>
               I am a 17 year old student, who likes doing sport, developing, and
-              reading books. In my freetime I often take a walk with my dog and
-              cycle through the surroundings of my hometown in
-              Northrhine-Westphalia.
+              reading books. I also love exploring the surroundings of my
+              hometown in Northrhine-Westphalia in Germany.
             </Typography>
           </TimelineOppositeContent>
           <TimelineSeparator></TimelineSeparator>
           <TimelineContent>
             <img
               src={Image}
-              height="466px"
-              width="746px"
+              height="466vh"
+              width="746vw"
               alt="Img not found"
               className={styles.image}
             ></img>
@@ -68,13 +82,55 @@ export default function Home(props: HomeProps): JSX.Element {
         <TimelineItem>
           <TimelineContent>
             <h3 className={styles.title}> My life </h3>
-            <Typography className={styles.info_text}> {"<text>"} </Typography>
+            <Typography className={styles.info_text}>
+              I love going outside and talking walks with my dog Simba through
+              the forest in my hometown Brochterbeck. Also I cycle much with my
+              bike through the countryside. Meeting friends and spending time
+              with them is another thing i like doing in my freetime. In
+              addition to that I am also member of the youth fire brigade in
+              Tecklenburg.
+            </Typography>
           </TimelineContent>
           <TimelineSeparator></TimelineSeparator>
 
           <TimelineOppositeContent>
-            <h3 className={styles.title}> I am a placeholder</h3>
+            <img
+              height="466vh"
+              width="746vw"
+              className={styles.image_left}
+              alt="Img not found"
+              src={Image2}
+            ></img>
           </TimelineOppositeContent>
+        </TimelineItem>
+        <TimelineItem key="teckdigital">
+          <TimelineOppositeContent>
+            <h3 className={styles.title}> TECKdigital </h3>
+            <Typography className={styles.info_text}>
+              {" "}
+              Since December 2019 I am a member of the at the begin of 2019
+              founded student company{" "}
+              <a href="http://teckdigital.de" className={styles.info_text}>
+                TECKdigital
+              </a>
+              . At TECKdigital I worked on the{" "}
+              <a href="/projects#teckboard" className={styles.projectLink}>
+                TECKboard
+              </a>
+              . By working there I got more knowledge about working in a team
+              and informatic in general.
+            </Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator></TimelineSeparator>
+          <TimelineContent>
+            <img
+              height="466vh"
+              width="746vw"
+              className={styles.image_right}
+              alt="Img not found"
+              src="https://asc-images.imgix.net/2021/5/7/172b0bbf-eb3e-4ce3-818e-c25fadd3c4d3.jpeg?w=2048&auto=format"
+            ></img>
+          </TimelineContent>
         </TimelineItem>
       </Timeline>
     </div>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,51 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
+import {
+  Timeline,
+  TimelineContent,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+} from "@material-ui/lab";
 import React from "react";
+import Typing from "react-typing-animation";
+import TECKboardApp from "../../assets/teckboard_app.png";
+import DevLights from "../../assets/devlights.png";
+//@ts-ignore
+import PDF from "../../assets/facharbeit.pdf";
 
 const useStyles = makeStyles(() => ({
   text: {
-    fontSize: 30,
+    fontSize: "8vh",
     fontWeight: "bold",
     alignSelf: "flex-start",
+  },
+  title: {
+    textAlign: "center",
+    alignSelf: "center",
+    color: "#fff",
+    textDecorationLine: "none",
+  },
+  image: {
+    borderRadius: 15,
+    transform: "rotate(0.025turn) translate(-100%, 10%)",
+  },
+  image_left: {
+    transform: "rotate(-0.025turn) translate(100%, 10%)",
+  },
+  info_text: {
+    fontSize: "2.5vh",
+    color: "#fff",
+    textAlign: "center",
+    width: window.innerWidth * 0.3,
+    overflowWrap: "break-word",
+  },
+  button: {
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  item: {
+    alignContent: "center",
+    alignItems: "center",
   },
 }));
 
@@ -14,9 +54,83 @@ export default function Projects(props: ProjectsProps): JSX.Element {
   const styles = useStyles();
   return (
     <div>
-      <Typography className={styles.text}>
-        Here you will find something about my projects{" "}
-      </Typography>
+      <Typing>
+        <Typography className={styles.text}>
+          The projects I developed!
+        </Typography>
+      </Typing>
+      <Timeline align="alternate">
+        <TimelineItem key="teckboard">
+          <TimelineOppositeContent>
+            <a
+              id="user-content-teckboard"
+              className={styles.title}
+              href={"#teckboard"}
+            >
+              <h3 about="TECKboard" className={styles.title}>
+                TECKboards
+              </h3>
+            </a>
+            <Typography className={styles.info_text}>
+              TECKboard is a realtime information system that two friends of
+              mine, Timo Peters and Yannick Hahn, developed. I spend really much
+              hours developing a smartphone application for this realtime
+              information system. By using the principe learning by doing, I
+              learned much about planing, developing and communicating with the
+              two developers of this system.
+              <br />
+              <a
+                href="https://teckboard.de"
+                target="_blank"
+                className={styles.info_text}
+              >
+                {" "}
+                Learn more
+              </a>
+            </Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator></TimelineSeparator>
+          <TimelineContent>
+            <img
+              height="65%"
+              width="auto"
+              className={styles.image}
+              src={TECKboardApp}
+              alt="TECKboard App"
+            ></img>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineContent className={styles.item}>
+            <h3 className={styles.title}> DevLights </h3>
+            <Typography className={styles.info_text}>
+              DevLights are the smart home LED Stripes for Developers. Together
+              with two friends of mine, Jaan Springer and Timo Peters, we
+              developed these lights during our project course informatic at
+              Graf-Adolf-Gymnasium. From begin we learned new programming
+              languages like C++ and worked at Hard and Software.
+            </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={styles.button}
+              onClick={() => window.open(PDF, "_blank")}
+            >
+              READ DOCUMENTATION
+            </Button>
+          </TimelineContent>
+          <TimelineSeparator />
+          <TimelineOppositeContent>
+            <img
+              height="65%"
+              width="auto"
+              className={styles.image_left}
+              alt="DevLights App"
+              src={DevLights}
+            />
+          </TimelineOppositeContent>
+        </TimelineItem>
+      </Timeline>
     </div>
   );
 }
