3616f4d07e76a4d66b10abed8733e8b228dadc8e
Jozys <joshua@slaar.de>
2023-09-23 21:30:07 +0200

    Added StyledEngineProvider
    - StyledEngineProvider needed to add Custom Style
    -  v4 injects custom styles automatically, v5 not

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import "./App.css";
 import Router from "./components/Router";
 import ThemeProvider from "./components/ThemeProvider";
 import LanguageProvider from "./components/LanguageProvider/LanguageProvider";
+import { StyledEngineProvider } from "@mui/material";
 
 function App() {
   return (
-    <div style={{ height: "100%", width: "100%" }}>
-      <LanguageProvider>
-        <ThemeProvider>
-          <Router />
-        </ThemeProvider>
-      </LanguageProvider>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <div style={{ height: "100%", width: "100%" }}>
+        <LanguageProvider>
+          <ThemeProvider>
+            <Router />
+          </ThemeProvider>
+        </LanguageProvider>
+      </div>
+    </StyledEngineProvider>
   );
 }
 
